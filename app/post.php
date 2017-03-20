99b79c5e43831797decc512e6e4a867243c644be
Christopher Vundi <chris.vundi@fire-engine-red.com>
2017-03-20 23:42:25 +0300

    Update post model

--- app/post.php
+++ app/post.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class post extends Model
+class Post extends Model
 {
     protected $fillable = [
         'user_id', 'title', 'description'
