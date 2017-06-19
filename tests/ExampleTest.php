3b171d49174a21278b795765efce2e046bb36402
Christopher Vundi <chris.vundi@fire-engine-red.com>
2017-06-19 17:23:00 +0300

    Fix test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $response = $this->get('/')
+             ->assertStatus(200);
     }
 }
