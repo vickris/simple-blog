59e305670c9350067afa2810c0d9bc6febc992e0
Christopher Vundi <chris.vundi@fire-engine-red.com>
2017-07-05 16:20:51 +0300

    Create a test for the index page

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,7 +11,7 @@ class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(10);
+        $posts = Post::latest()->paginate(10);
 
         return view('posts.index', compact('posts'));
     }
---
+++ tests/ViewPostsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewPostsTest extends TestCase
+{
+    use DatabaseTransactions;
+    /** @test */
+    public function users_can_see_all_posts()
+    {
+        $post = factory(App\Post::class)->create();
+
+        $this->actingAs(factory(User::class)->create())
+            ->get('/posts')
+            ->assertStatus(200)
+            ->assertSee($post->title);
+    }
+}
